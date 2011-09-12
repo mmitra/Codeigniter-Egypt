f6de1ea3183c5cb32988c3ea8a91dfc47d501ec8
Monty Mitra <mmitra@engineyard.com>
2011-09-12 10:05:42 -0700

    Edited config.php via GitHub

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 $config_site_name		= "Site Name";
 
 $db_hostname			= "10.194.111.8";
-$db_database			= "egypt";
+$db_database			= "db_3ccc50dc";
 $db_username			= "user_3ccc50dc";
 $db_password			= "MJFB@3FgFau),w";
 $db_dbdriver			= "mysql";
